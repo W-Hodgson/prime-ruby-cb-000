3d59df3dbb0531127a9e21bfb8b2b4b1114d3a2d
Will Hodgson <will.hodgson@current-rms.com>
2019-07-29 13:40:52 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,7 +9,8 @@ def prime?(number)
   if number == 2
     return true
   end
-  number_array = 3...number-1
+  panultimate = number - 1
+  number_array = 3...panultimate
   number_array.each do |num|
     if number % num == 0
       return false
